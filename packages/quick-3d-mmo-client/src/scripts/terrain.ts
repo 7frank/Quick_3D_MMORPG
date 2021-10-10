666c25d6917a3f244696d13983c18fb2c208a3a5
Frank Reimann <>
2021-10-10 15:03:50 +0200

    fix window resize

--- packages/quick-3d-mmo-client/src/scripts/terrain.ts
+++ packages/quick-3d-mmo-client/src/scripts/terrain.ts
@@ -1,13 +1,17 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-import { Component } from './entity';
-import { CubeQuadTree } from './quadtree';
-import { VS1, VS2, PS1, PS2 } from './terrain-shader';
-import { TerrainChunkRebuilder_Threaded } from './terrain-builder-threaded';
-import { TextureSplatter } from './texture-splatter';
-import { TextureAtlas } from './textures';
-import { utils } from './utils';
-import { Noise as aNoise, TerrainHeight, TerrainConstants } from 'quick-3d-mmo-shared';
+import { Component } from './entity'
+import { CubeQuadTree } from './quadtree'
+import { VS1, VS2, PS1, PS2 } from './terrain-shader'
+import { TerrainChunkRebuilder_Threaded } from './terrain-builder-threaded'
+import { TextureSplatter } from './texture-splatter'
+import { TextureAtlas } from './textures'
+import { utils } from './utils'
+import {
+  Noise as aNoise,
+  TerrainHeight,
+  TerrainConstants,
+} from 'quick-3d-mmo-shared'
 
 const {
   QT_MIN_CELL_SIZE,
@@ -19,31 +23,43 @@ const {
 } = { ...aNoise, ...TerrainHeight, ...TerrainConstants }
 
 class TerrainChunkManager extends Component {
-  _params: any;
-  _material: THREE.MeshStandardMaterial;
-  _builder: TerrainChunkRebuilder_Threaded;
-  heightGenerator_: any;
-  _biomes: any;
-  _biomesParams: any;
-  _colourNoise: any;
-  _colourNoiseParams: { octaves: number; persistence: number; lacunarity: number; exponentiation: number; scale: number; noiseType: string; seed: number; height: number; };
-  _groups: THREE.Group[];
-  _chunks: any;
+  _params: any
+  _material: THREE.MeshStandardMaterial
+  _builder: TerrainChunkRebuilder_Threaded
+  heightGenerator_: any
+  _biomes: any
+  _biomesParams: any
+  _colourNoise: any
+  _colourNoiseParams: {
+    octaves: number
+    persistence: number
+    lacunarity: number
+    exponentiation: number
+    scale: number
+    noiseType: string
+    seed: number
+    height: number
+  }
+  _groups: THREE.Group[]
+  _chunks: any
 
   constructor(params) {
-    super();
-    this._Init(params);
+    super()
+    this._Init(params)
   }
 
   _Init(params) {
-    this._params = params;
+    this._params = params
 
-    const loader = new THREE.TextureLoader();
+    const loader = new THREE.TextureLoader()
 
-    const noiseTexture = loader.load('src/resources/terrain/simplex-png');
-    noiseTexture.wrapS = THREE.RepeatWrapping;
-    noiseTexture.wrapT = THREE.RepeatWrapping;
-    const diffuse = new TextureAtlas(params);
+    const noiseTexture = loader.load('src/resources/terrain/simplex-png')
+    noiseTexture.wrapS = THREE.RepeatWrapping
+    noiseTexture.wrapT = THREE.RepeatWrapping
+    const diffuse = new TextureAtlas(params)
+
+    console.log('diffuse', diffuse)
+
     diffuse.Load('diffuse', [
       'src/resources/terrain/dirt_01_diffuse-1024.png',
       'src/resources/terrain/grass1-albedo3-1024.png',
@@ -54,10 +70,9 @@ class TerrainChunkManager extends Component {
       'src/resources/terrain/rough-wet-cobble-albedo-1024.png',
       // 'resources/terrain/sandy-rocks1-albedo-1024.png',
       'src/resources/terrain/bark1-albedo.jpg',
-    ]);
+    ])
 
-
-    const normal = new TextureAtlas(params);
+    const normal = new TextureAtlas(params)
     normal.Load('normal', [
       'src/resources/terrain/dirt_01_normal-1024.jpg',
       'src/resources/terrain/grass1-normal-1024.jpg',
@@ -68,58 +83,62 @@ class TerrainChunkManager extends Component {
       'src/resources/terrain/rough-wet-cobble-normal-1024.jpg',
       // 'src/resources/terrain/sandy-rocks1-normal-1024.jpg',
       'src/resources/terrain/bark1-normal3.jpg',
-    ]);
+    ])
 
     this._material = new THREE.MeshStandardMaterial({
       side: THREE.BackSide,
       vertexColors: true,
-    });
+    })
 
     this._material.onBeforeCompile = (s) => {
-      let a = 0;
-      let vsh = s.vertexShader;
-      vsh = VS1 + s.vertexShader;
-      const vi1 = vsh.search('#include <fog_vertex>');
-      vsh = [vsh.slice(0, vi1) + VS2 + vsh.slice(vi1)].join('');
-      s.vertexShader = vsh;
+      let a = 0
+      let vsh = s.vertexShader
+      vsh = VS1 + s.vertexShader
+      const vi1 = vsh.search('#include <fog_vertex>')
+      vsh = [vsh.slice(0, vi1) + VS2 + vsh.slice(vi1)].join('')
+      s.vertexShader = vsh
 
-      s.fragmentShader = PS1 + s.fragmentShader;
-      const fi1 = s.fragmentShader.search('#include <lights_physical_fragment>');
-      s.fragmentShader = [s.fragmentShader.slice(0, fi1) + PS2 + s.fragmentShader.slice(fi1)].join('');
+      s.fragmentShader = PS1 + s.fragmentShader
+      const fi1 = s.fragmentShader.search('#include <lights_physical_fragment>')
+      s.fragmentShader = [
+        s.fragmentShader.slice(0, fi1) + PS2 + s.fragmentShader.slice(fi1),
+      ].join('')
 
-      s.uniforms.TRIPLANAR_normalMap = { value: normal.Info['normal']?.atlas };
-      s.uniforms.TRIPLANAR_diffuseMap = { value: diffuse.Info['diffuse']?.atlas };
-      s.uniforms.TRIPLANAR_noiseMap = { value: noiseTexture };
+      s.uniforms.TRIPLANAR_normalMap = { value: normal.Info['normal']?.atlas }
+      s.uniforms.TRIPLANAR_diffuseMap = {
+        value: diffuse.Info['diffuse']?.atlas,
+      }
+      s.uniforms.TRIPLANAR_noiseMap = { value: noiseTexture }
 
       diffuse.onLoad = () => {
-        s.uniforms.TRIPLANAR_diffuseMap.value = diffuse.Info['diffuse'].atlas;
-      };
+        s.uniforms.TRIPLANAR_diffuseMap.value = diffuse.Info['diffuse'].atlas
+      }
       normal.onLoad = () => {
-        s.uniforms.TRIPLANAR_normalMap.value = normal.Info['normal'].atlas;
-      };
+        s.uniforms.TRIPLANAR_normalMap.value = normal.Info['normal'].atlas
+      }
 
       // s.fragmentShader += 'poop';
-    };
+    }
 
-    this._builder = new TerrainChunkRebuilder_Threaded(null);
+    this._builder = new TerrainChunkRebuilder_Threaded(null)
     // this._builder = new terrain_builder.TerrainChunkRebuilder();
 
-    this._InitNoise();
+    this._InitNoise()
     // if(!params?.guiParams) {
     //   console.log("GUIParams undefined")
     //   return;
     // }
-    this._InitBiomes(params);
-    this._InitTerrain(params);
+    this._InitBiomes(params)
+    this._InitTerrain(params)
   }
 
   _InitNoise() {
-    this.heightGenerator_ = new HeightGenerator();
+    this.heightGenerator_ = new HeightGenerator()
   }
 
   _InitBiomes(params) {
     if (!params?.guiParams) {
-      console.error("terrainManager guiParamsMissing")
+      console.error('terrainManager guiParamsMissing')
     }
     params.guiParams.biomes = {
       octaves: 2,
@@ -129,12 +148,12 @@ class TerrainChunkManager extends Component {
       noiseType: 'simplex',
       seed: 2,
       exponentiation: 2,
-      height: 1.0
-    };
+      height: 1.0,
+    }
 
     const onNoiseChanged = () => {
-      this._builder.Rebuild(this._chunks);
-    };
+      this._builder.Rebuild(this._chunks)
+    }
 
     // const noiseRollup = params.gui.addFolder('resources/Terrain.Biomes');
     // noiseRollup.add(params.guiParams.biomes, "scale", 64.0, 4096.0).onChange(
@@ -148,8 +167,8 @@ class TerrainChunkManager extends Component {
     // noiseRollup.add(params.guiParams.biomes, "exponentiation", 0.1, 10.0).onChange(
     //   onNoiseChanged);
 
-    this._biomes = new Noise(params.guiParams.biomes);
-    this._biomesParams = params.guiParams.biomes;
+    this._biomes = new Noise(params.guiParams.biomes)
+    this._biomesParams = params.guiParams.biomes
 
     const colourParams = {
       octaves: 1,
@@ -160,19 +179,19 @@ class TerrainChunkManager extends Component {
       noiseType: 'simplex',
       seed: 2,
       height: 1.0,
-    };
-    this._colourNoise = new Noise(colourParams);
-    this._colourNoiseParams = colourParams;
+    }
+    this._colourNoise = new Noise(colourParams)
+    this._colourNoiseParams = colourParams
   }
 
   _InitTerrain(params) {
     if (!params?.guiParams)
       params.guiParams.terrain = {
         wireframe: false,
-      };
+      }
 
-    this._groups = [...new Array(6)].map(_ => new THREE.Group());
-    params.scene.add(...this._groups);
+    this._groups = [...new Array(6)].map((_) => new THREE.Group())
+    params.scene.add(...this._groups)
 
     // const terrainRollup = params.gui.addFolder('resources/Terrain');
     // terrainRollup.add(params.guiParams.terrain, "wireframe").onChange(() => {
@@ -181,8 +200,8 @@ class TerrainChunkManager extends Component {
     //   }
     // });
 
-    this._chunks = {};
-    this._params = params;
+    this._chunks = {}
+    this._params = params
   }
 
   _CreateTerrainChunk(group, groupTransform, offset, width, resolution) {
@@ -196,8 +215,10 @@ class TerrainChunkManager extends Component {
       radius: PLANET_RADIUS,
       resolution: resolution,
       biomeGenerator: this._biomes,
-      colourGenerator: new TextureSplatter(
-        { biomeGenerator: this._biomes, colourNoise: this._colourNoise }),
+      colourGenerator: new TextureSplatter({
+        biomeGenerator: this._biomes,
+        colourNoise: this._colourNoise,
+      }),
       heightGenerators: [this.heightGenerator_],
       noiseParams: NOISE_PARAMS,
       colourNoiseParams: this._colourNoiseParams,
@@ -209,38 +230,41 @@ class TerrainChunkManager extends Component {
       heightGeneratorsParams: {
         min: 100000,
         max: 100000 + 1,
-      }
-    };
+      },
+    }
 
-    return this._builder.AllocateChunk(params);
+    return this._builder.AllocateChunk(params)
   }
 
   GetHeight(pos) {
-    return this.heightGenerator_.Get(pos.x, 0.0, pos.z);
+    return this.heightGenerator_.Get(pos.x, 0.0, pos.z)
   }
 
   GetBiomeAt(pos) {
-    return this._biomes.Get(pos.x, 0.0, pos.z);
+    return this._biomes.Get(pos.x, 0.0, pos.z)
   }
 
   Update(_) {
     // console.log("TerrainChunkManager.Update()", this._params)
-    const target = this.FindEntity(this._params.target);
+    const target = this.FindEntity(this._params.target)
     if (!target) {
-      console.warn("TerrainChunkManager.Update Cannot find Target Entity: ", this._params.target)
-      return;
+      console.warn(
+        'TerrainChunkManager.Update Cannot find Target Entity: ',
+        this._params.target,
+      )
+      return
     }
 
-    this._builder.Update();
+    this._builder.Update()
     if (!this._builder.Busy) {
-      this._UpdateVisibleChunks_Quadtree(target);
+      this._UpdateVisibleChunks_Quadtree(target)
     }
 
     for (let k in this._chunks) {
-      this._chunks[k].chunk.Update(target.Position);
+      this._chunks[k].chunk.Update(target.Position)
     }
     for (let c of this._builder._old) {
-      c.chunk.Update(target.Position);
+      c.chunk.Update(target.Position)
     }
 
     // this._params.scattering.uniforms.planetRadius.value = PLANET_RADIUS;
@@ -250,24 +274,24 @@ class TerrainChunkManager extends Component {
   _UpdateVisibleChunks_Quadtree(target) {
     // console.log("TerrainChunkManager._UpdateVisibleChunks_Quadtree()", target)
     function _Key(c) {
-      return c.position[0] + '/' + c.position[2] + ' [' + c.size + ']';
+      return c.position[0] + '/' + c.position[2] + ' [' + c.size + ']'
     }
 
     const q = new CubeQuadTree({
       radius: PLANET_RADIUS,
       min_node_size: QT_MIN_CELL_SIZE,
-    });
-    q.Insert(target.Position);
+    })
+    q.Insert(target.Position)
 
-    const sides = q.GetChildren();
+    const sides = q.GetChildren()
 
-    let newTerrainChunks = {};
-    const center = new THREE.Vector3();
-    const dimensions = new THREE.Vector3();
+    let newTerrainChunks = {}
+    const center = new THREE.Vector3()
+    const dimensions = new THREE.Vector3()
     for (let i = 0; i < sides.length; i++) {
       for (let c of sides[i].children) {
-        c.bounds.getCenter(center);
-        c.bounds.getSize(dimensions);
+        c.bounds.getCenter(center)
+        c.bounds.getSize(dimensions)
 
         const child = {
           index: i,
@@ -276,38 +300,41 @@ class TerrainChunkManager extends Component {
           position: [center.x, center.y, center.z],
           bounds: c.bounds,
           size: dimensions.x,
-        };
+        }
 
-        const k = _Key(child);
-        newTerrainChunks[k] = child;
+        const k = _Key(child)
+        newTerrainChunks[k] = child
       }
     }
 
-    const intersection = utils.DictIntersection(this._chunks, newTerrainChunks);
-    const difference = utils.DictDifference(newTerrainChunks, this._chunks);
-    const recycle = Object.values(utils.DictDifference(this._chunks, newTerrainChunks));
+    const intersection = utils.DictIntersection(this._chunks, newTerrainChunks)
+    const difference = utils.DictDifference(newTerrainChunks, this._chunks)
+    const recycle = Object.values(
+      utils.DictDifference(this._chunks, newTerrainChunks),
+    )
 
-    this._builder.RetireChunks(recycle);
+    this._builder.RetireChunks(recycle)
 
-    newTerrainChunks = intersection;
+    newTerrainChunks = intersection
 
     for (let k in difference) {
-      const [xp, yp, zp] = difference[k].position;
+      const [xp, yp, zp] = difference[k].position
 
-      const offset = new THREE.Vector3(xp, yp, zp);
+      const offset = new THREE.Vector3(xp, yp, zp)
       newTerrainChunks[k] = {
         position: [xp, zp],
         chunk: this._CreateTerrainChunk(
-          difference[k].group, difference[k].transform,
-          offset, difference[k].size,
-          QT_MIN_CELL_RESOLUTION),
-      };
+          difference[k].group,
+          difference[k].transform,
+          offset,
+          difference[k].size,
+          QT_MIN_CELL_RESOLUTION,
+        ),
+      }
     }
 
-    this._chunks = newTerrainChunks;
+    this._chunks = newTerrainChunks
   }
 }
 
-export {
-  TerrainChunkManager
-}
+export { TerrainChunkManager }
--- packages/quick-3d-mmo-client/src/scripts/threejs_component.ts
+++ packages/quick-3d-mmo-client/src/scripts/threejs_component.ts
@@ -1,10 +1,10 @@
-import * as THREE from 'three';
+import * as THREE from 'three'
 
-import { Constants } from 'quick-3d-mmo-shared';
-import { Component } from "./entity";
-import { KNOWN_ENTITIES } from 'quick-3d-mmo-shared/src/constants';
+import { Constants } from 'quick-3d-mmo-shared'
+import { Component } from './entity'
+import { KNOWN_ENTITIES } from 'quick-3d-mmo-shared/src/constants'
 
-const { DOM_IDS } = Constants;
+const { DOM_IDS } = Constants
 
 const _VS = `
   varying vec3 vWorldPosition;
@@ -14,8 +14,7 @@ const _VS = `
     vWorldPosition = worldPosition.xyz;
   
     gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
-  }`;
-
+  }`
 
 const _FS = `
   uniform vec3 topColor;
@@ -37,17 +36,23 @@ const _FS = `
   
     vec3 sky = mix(stars, bottomColor, f);
     gl_FragColor = vec4(sky, 1.0);
-  }`;
+  }`
 
 class ThreeJSController extends Component {
-  threejs_: THREE.WebGLRenderer;
-  camera_: THREE.PerspectiveCamera;
-  scene_: THREE.Scene;
-  sun_: THREE.DirectionalLight;
-  _onWindowResize: any;
+  threejs_: THREE.WebGLRenderer
+  camera_: THREE.PerspectiveCamera
+  scene_: THREE.Scene
+  sun_: THREE.DirectionalLight
+
+  _OnWindowResize() {
+    // TODO find out what camera handling would have been done inside terrain in addition to the main.ts resize handler
+    // this.camera_.aspect = window.innerWidth / window.innerHeight
+    //  this.camera_.updateProjectionMatrix()
+    //  this.threejs_.setSize(window.innerWidth, window.innerHeight)
+  }
 
   constructor() {
-    super();
+    super()
   }
 
   InitEntity() {
@@ -65,7 +70,7 @@ class ThreeJSController extends Component {
         fogFactor = saturate(fogFactor);
   
         gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, fogFactor );
-      #endif`;
+      #endif`
 
     THREE.ShaderChunk.fog_pars_fragment = `
       #ifdef USE_FOG
@@ -78,73 +83,76 @@ class ThreeJSController extends Component {
           uniform float fogNear;
           uniform float fogFar;
         #endif
-      #endif`;
+      #endif`
 
     THREE.ShaderChunk.fog_vertex = `
       #ifdef USE_FOG
         vWorldPosition = (modelMatrix * vec4(transformed, 1.0 )).xyz;
-      #endif`;
+      #endif`
 
     THREE.ShaderChunk.fog_pars_vertex = `
       #ifdef USE_FOG
         varying vec3 vWorldPosition;
-      #endif`;
+      #endif`
 
     this.threejs_ = new THREE.WebGLRenderer({
       antialias: false,
-    });
-    this.threejs_.outputEncoding = THREE.sRGBEncoding;
-    this.threejs_.gammaFactor = 2.2;
-    this.threejs_.shadowMap.enabled = true;
-    this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap;
-    this.threejs_.setPixelRatio(window.devicePixelRatio);
-    this.threejs_.setSize(window.innerWidth, window.innerHeight);
-    this.threejs_.domElement.id = DOM_IDS.THREEJS;
+    })
+    this.threejs_.outputEncoding = THREE.sRGBEncoding
+    this.threejs_.gammaFactor = 2.2
+    this.threejs_.shadowMap.enabled = true
+    this.threejs_.shadowMap.type = THREE.PCFSoftShadowMap
+    this.threejs_.setPixelRatio(window.devicePixelRatio)
+    this.threejs_.setSize(window.innerWidth, window.innerHeight)
+    this.threejs_.domElement.id = DOM_IDS.THREEJS
 
-    document.getElementById('container').appendChild(this.threejs_.domElement);
+    document.getElementById('container').appendChild(this.threejs_.domElement)
 
-    window.addEventListener('resize', () => {
-      this._onWindowResize();
-    }, false);
+    window.addEventListener(
+      'resize',
+      () => {
+        this._OnWindowResize()
+      },
+      false,
+    )
 
-    const fov = 60;
-    const aspect = 1920 / 1080;
-    const near = 1.0;
-    const far = 5000.0;
-    this.camera_ = new THREE.PerspectiveCamera(fov, aspect, near, far);
-    this.camera_.position.set(25, 10, 25);
+    const fov = 60
+    const aspect = 1920 / 1080
+    const near = 1.0
+    const far = 5000.0
+    this.camera_ = new THREE.PerspectiveCamera(fov, aspect, near, far)
+    this.camera_.position.set(25, 10, 25)
 
-    this.scene_ = new THREE.Scene();
-    this.scene_.fog = new THREE.FogExp2(0x89b2eb, 0.00005);
+    this.scene_ = new THREE.Scene()
+    this.scene_.fog = new THREE.FogExp2(0x89b2eb, 0.00005)
 
-    let light = new THREE.DirectionalLight(0x8088b3, 0.7);
-    light.position.set(-10, 500, 10);
-    light.target.position.set(0, 0, 0);
-    light.castShadow = true;
-    light.shadow.bias = -0.001;
-    light.shadow.mapSize.width = 4096;
-    light.shadow.mapSize.height = 4096;
-    light.shadow.camera.near = 0.1;
-    light.shadow.camera.far = 1000.0;
-    light.shadow.camera.left = 100;
-    light.shadow.camera.right = -100;
-    light.shadow.camera.top = 100;
-    light.shadow.camera.bottom = -100;
-    this.scene_.add(light);
+    let light = new THREE.DirectionalLight(0x8088b3, 0.7)
+    light.position.set(-10, 500, 10)
+    light.target.position.set(0, 0, 0)
+    light.castShadow = true
+    light.shadow.bias = -0.001
+    light.shadow.mapSize.width = 4096
+    light.shadow.mapSize.height = 4096
+    light.shadow.camera.near = 0.1
+    light.shadow.camera.far = 1000.0
+    light.shadow.camera.left = 100
+    light.shadow.camera.right = -100
+    light.shadow.camera.top = 100
+    light.shadow.camera.bottom = -100
+    this.scene_.add(light)
 
-    this.sun_ = light;
+    this.sun_ = light
 
-    this.LoadSky_();
+    this.LoadSky_()
   }
 
   LoadSky_() {
-    const hemiLight = new THREE.HemisphereLight(0x424a75, 0x6a88b5, 0.7);
+    const hemiLight = new THREE.HemisphereLight(0x424a75, 0x6a88b5, 0.7)
     // hemiLight.color.setHSL(0.6, 1, 0.4);
     // hemiLight.groundColor.setHSL(0.095, 1, 0.5);
-    this.scene_.add(hemiLight);
+    this.scene_.add(hemiLight)
 
-
-    const loader = new THREE.CubeTextureLoader();
+    const loader = new THREE.CubeTextureLoader()
     const texture = loader.load([
       'src/resources/terrain/space-posx.jpg',
       'src/resources/terrain/space-negx.jpg',
@@ -152,48 +160,45 @@ class ThreeJSController extends Component {
       'src/resources/terrain/space-negy.jpg',
       'src/resources/terrain/space-posz.jpg',
       'src/resources/terrain/space-negz.jpg',
-    ]);
-    texture.encoding = THREE.sRGBEncoding;
+    ])
+    texture.encoding = THREE.sRGBEncoding
 
     const uniforms = {
-      "topColor": { value: new THREE.Color(0x000000) },
-      "bottomColor": { value: new THREE.Color(0x5d679e) },
-      "offset": { value: -500 },
-      "exponent": { value: 0.3 },
-      "background": { value: texture },
-    };
+      topColor: { value: new THREE.Color(0x000000) },
+      bottomColor: { value: new THREE.Color(0x5d679e) },
+      offset: { value: -500 },
+      exponent: { value: 0.3 },
+      background: { value: texture },
+    }
     // uniforms["topColor"].value.copy(hemiLight.color);
 
-    this.scene_.fog.color.copy(uniforms["bottomColor"].value);
+    this.scene_.fog.color.copy(uniforms['bottomColor'].value)
 
-
-    const skyGeo = new THREE.SphereBufferGeometry(5000, 32, 15);
+    const skyGeo = new THREE.SphereBufferGeometry(5000, 32, 15)
     const skyMat = new THREE.ShaderMaterial({
       uniforms: uniforms,
       vertexShader: _VS,
       fragmentShader: _FS,
-      side: THREE.BackSide
-    });
+      side: THREE.BackSide,
+    })
 
-    const sky = new THREE.Mesh(skyGeo, skyMat);
-    this.scene_.add(sky);
+    const sky = new THREE.Mesh(skyGeo, skyMat)
+    this.scene_.add(sky)
   }
 
   Update(_) {
-    const player = this.FindEntity(KNOWN_ENTITIES.PLAYER);
+    const player = this.FindEntity(KNOWN_ENTITIES.PLAYER)
     if (!player) {
-      return;
+      return
     }
-    const pos = player._position;
+    const pos = player._position
 
-    this.sun_.position.copy(pos);
-    this.sun_.position.add(new THREE.Vector3(-50, 200, -10));
-    this.sun_.target.position.copy(pos);
-    this.sun_.updateMatrixWorld();
-    this.sun_.target.updateMatrixWorld();
+    this.sun_.position.copy(pos)
+    this.sun_.position.add(new THREE.Vector3(-50, 200, -10))
+    this.sun_.target.position.copy(pos)
+    this.sun_.updateMatrixWorld()
+    this.sun_.target.updateMatrixWorld()
   }
 }
 
-export {
-  ThreeJSController
-}
+export { ThreeJSController }
